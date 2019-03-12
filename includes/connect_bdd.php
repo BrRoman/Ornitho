5687b35d32265886240c1a67b27ae702ad121cf6
BrRoman <editeur@traditions-monastiques.com>
2019-03-12 08:41:32 +0100

    Static site.

--- includes/connect_bdd.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$bdd = new PDO("mysql:host=localhost;dbname=ornitho;charset=utf8", "root", "sql", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-?>
-
--- includes/head.php
+++
@@ -1,5 +0,0 @@
-<meta charset="utf-8" />
-<link rel="icon" sizes="200x200" href="images/ornitho.png" />
-<link rel="stylesheet" href="styles.css" />
-<title>Ornitho - Voir le parc autrement</title>
-
